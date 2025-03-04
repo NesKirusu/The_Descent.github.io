ad4a70bccba5f1ad06f59ace01a6b2d524666e0e
Christian Barretto <angkevin33@gmail.com>
2025-03-04 13:10:43 +0800

    2.1.1

--- app.js
+++ app.js
@@ -3,32 +3,18 @@ window.addEventListener('scroll', ()=>{
     let value = window.scrollY
     let hydroflask_img = document.getElementById('hydroflask')
     let screen_width = window.innerWidth
+    let screen_height = window.innerHeight
     text_margintop.style.marginTop = value*1.2 +'px'
     console.log(value)
-    if(value >= 450){
-        gsap.to(hydroflask_img, {
-            x:screen_width*-0.6,
-            y:850,
-            rotation:-20,
-            duration:0.5
-        })
-    }
-    else{
+    if(value < 450){
         gsap.to(hydroflask_img, {
             x:0,
             y:0,
-            rotation:0,
-            duration:0.5
+            opacity: 1,
+            rotation:0
         })
     }
-    if(value >= 1040){
-        gsap.to(hydroflask_img, {
-            x: screen_width*-0.5,
-            y: 1240,
-            opacity: 0,     
-        })
-    }
-    else if(value >=450){
+    if(value > 450 && value < 1040){
         gsap.to(hydroflask_img, {
             x:screen_width*-0.6,
             y:850,
